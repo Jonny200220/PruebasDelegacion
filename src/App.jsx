71fc10f408e616847327f0770364539cc3173c92
Jonny <jonnyrmzhdz@gmail.com>
2024-09-23 12:15:47 -0600

    Reabase del repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Tabla from './components/Table'
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>MI TABLA</p>
+      <Tabla/>
+    </>
+  )
+}
+
+export default App
